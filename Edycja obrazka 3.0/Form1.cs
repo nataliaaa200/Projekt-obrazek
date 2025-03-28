7c16278e4ef222465ef2747fa8e268653eb39146
Natalia <natalia.kus@student.put.poznan.pl>
2025-03-29 00:57:11 +0100

    Wersja merge

--- Edycja obrazka 3.0/Form1.cs
+++ Edycja obrazka 3.0/Form1.cs
@@ -2,10 +2,9 @@ namespace Edycja_obrazka_3._0
 {
     public partial class Form1 : Form
     {
-        private Bitmap? originalImage = null;
+        private Bitmap? originalImage = null; // Nullable, aby unikn¹æ problemów z null
         private bool isGreenFilterApplied = false;
-        private Bitmap originalImage = null;
-        private bool isGreenFilterApplied = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -17,7 +16,6 @@ namespace Edycja_obrazka_3._0
             openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png|All Files|*.*";  // Mo¿na dodaæ inne formaty plików
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-
                 pictureBox.Image = new Bitmap(openFileDialog.FileName);
             }
         }
@@ -26,9 +24,9 @@ namespace Edycja_obrazka_3._0
         {
             if (pictureBox.Image == null) return;
 
-            if (!isGreenFilterApplied) 
+            if (!isGreenFilterApplied)
             {
-                originalImage = new Bitmap(pictureBox.Image); 
+                originalImage = new Bitmap(pictureBox.Image);
                 Bitmap bmp = new Bitmap(originalImage);
 
                 for (int y = 0; y < bmp.Height; y++)
@@ -46,7 +44,7 @@ namespace Edycja_obrazka_3._0
                 pictureBox.Image = bmp;
                 isGreenFilterApplied = true;
             }
-            else 
+            else
             {
                 pictureBox.Image = new Bitmap(originalImage);
                 isGreenFilterApplied = false;
@@ -88,36 +86,6 @@ namespace Edycja_obrazka_3._0
             {
                 pictureBox.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
                 pictureBox.Refresh();
-            }
-        }
-        private void btnOnlyGreen_Click(object sender, EventArgs e)
-        {
-            if (pictureBox.Image == null) return;
-
-            if (!isGreenFilterApplied) // Jeœli filtr nie jest zastosowany, stosujemy go
-            {
-                originalImage = new Bitmap(pictureBox.Image); // Zapisujemy oryginalny obraz
-                Bitmap bmp = new Bitmap(originalImage);
-
-                for (int y = 0; y < bmp.Height; y++)
-                {
-                    for (int x = 0; x < bmp.Width; x++)
-                    {
-                        Color pixel = bmp.GetPixel(x, y);
-
-                        if (!(pixel.G > pixel.R && pixel.G > pixel.B))
-                        {
-                            bmp.SetPixel(x, y, Color.Black);
-                        }
-                    }
-                }
-                pictureBox.Image = bmp;
-                isGreenFilterApplied = true;
-            }
-            else // Jeœli filtr jest ju¿ zastosowany, przywracamy oryginalny obraz
-            {
-                pictureBox.Image = new Bitmap(originalImage);
-                isGreenFilterApplied = false;
             }
         }
     }
